ceecc7067f5ca31ffa34ee3b61a3e8fb58322216
Subham Raj <rajshubham302@gmail.com>
2025-10-18 21:34:14 +0530

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stl-collections-guide/', // <-- Make sure this line exists and is correct
+  base: '/stl-collections-guide/',
 })
