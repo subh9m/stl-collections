d98118331f4ce87d1266cc2469c1e679752d86b0
Subham Raj <rajshubham302@gmail.com>
2025-10-19 20:40:10 +0530

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/stl-collections/', // ⚠️ change this to match your new repo name
-})
