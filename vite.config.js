4406565aeae941ff3990ab9cc9167909f0551d0e
Subham Raj <rajshubham302@gmail.com>
2025-10-19 07:46:26 +0530

    Initial commit for STL Collections

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stl-collections-guide/',
+  base: '/stl-collections/', // ⚠️ change this to match your new repo name
 })
